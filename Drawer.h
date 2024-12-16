4fa6fedc93b0bb0b163365b5d90608d581e6fe42
sodene <giaobao2kk5@gmail.com>
2024-12-16 12:00:07 +0700

    Linear Gradient pushing

--- Drawer.h
+++ Drawer.h
@@ -3,6 +3,7 @@
 
 #include "objects.h"
 #include "Path.h"
+#include "Gradient.h"
 #include "gdiplus.h"
 
 using namespace Gdiplus;
@@ -15,23 +16,38 @@ private:
     Gdiplus::PointF anchor;
     Pen* p;
     SolidBrush* b;
+    LinearGradientBrush* gb;
     Gdiplus::Matrix Ma;
+    LinearVector gradientList;
 public:
-    Drawer(vector<unique_ptr<Shapes::Object>>& list, Gdiplus::Graphics* g, float s, Gdiplus::PointF anchor);
-    
+    Drawer(vector<unique_ptr<Shapes::Object>>& list, Gdiplus::Graphics* g, float s, Gdiplus::PointF anchor, LinearVector linear);
+    void Reset();
     void setDrawer(Shapes::Object* obj);
     void DrawR(Shapes::Object* obj);
+    void FillRectGradient(Shapes::Rectangle* R);
+
     void DrawL(Shapes::Object* obj);
+
     void DrawC(Shapes::Object* obj);
+    void FillCircleGradient(Shapes::Circle* C);
+
     void DrawE(Shapes::Object* obj);
+    void FillEllipseGradient(Shapes::Ellipse* E);
+
     void DrawPG(Shapes::Object* obj);
+    void FillPGGradient(Shapes::Polygon* PG,  Gdiplus::GraphicsPath* path);
+
     void DrawPL(Shapes::Object* obj);
+    // void FillPLGradient(Shapes::Polyline* PL);
+
     void DrawT(Shapes::Object* obj);
+    // void FillTextGradient(Shapes::Rectangle* T);
+
     void DrawP(Shapes::Object* obj);
+    void FillPGradient(Shapes::Path* P, Gdiplus::GraphicsPath* path);
+
     void DrawG(Shapes::Object* obj);
     void Draw();
+    void setGradientBrush(string r, float alpha);
 };    
-
-
-
 #endif
